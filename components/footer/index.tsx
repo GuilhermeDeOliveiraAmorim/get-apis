4941ce62e7f7ee94ca0b09a59eae24e6863009bd
guilhermeamorim <guilhermeamorim@deso-se.com.br>
2022-08-08 13:47:36 -0300

    feat: add footer

---
+++ components/footer/index.tsx
@@ -0,0 +1,11 @@
+import FooterStyle from "./Footer.module.css";
+
+export default function Footer() {
+    return (
+        <div className={FooterStyle.footer}>
+            <h1>
+                Footer
+            </h1>
+        </div>
+    );
+}
